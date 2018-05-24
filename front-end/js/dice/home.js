d5d74e3a9802d4d96e80acc9cd3d7733ae7b70b5
Yang Li <liyang0520@hotmail.com>
2018-05-24 06:13:28 -0600

    front-end change

--- front-end/js/dice/home.js
+++ front-end/js/dice/home.js
@@ -53,31 +53,51 @@ var HttpService = function() {
 var dice = {
 	ajax: new HttpService(),
 	dicewait:0,
-	account:1000,
+	account:0,
 	bet_value:0,
-	current_term:267,//TODO
+	current_term:0,
 	bet:0,
+	bet_name:'无',
 	select_num: function (number,name){
-		$('#account').text(this.account);
-		$('#bet_value').html(this.bet_value);
-		$('#bet_name').text(name);
-		this.bet = number;
-		
-		layer.open({
-		  type: 1,
-		  title: false,
-		  closeBtn: 0,
-		  shadeClose: true,
-		  area: '420px',
-		  skin: 'bet-skin',
-		  content: $('#bet'),
-		  end: function(){
-			if(dice.bet_value !=0){
-				dice.account = dice.account + dice.bet_value;
-				dice.bet_value = 0;
+			this.ajax.get('/dice/account-info', {}, function(data) {
+			if (data.code == '2018') {
+				dice.account = data.result.account;
+				$('#account').text(dice.account);
+				$('#bet_value').html(dice.bet_value);
+				$('#bet_name').text(name);
+				dice.bet = number;
+				dice.bet_name = name;
+				layer.open({
+				  type: 1,
+				  title: false,
+				  closeBtn: 0,
+				  shadeClose: true,
+				  area: '420px',
+				  skin: 'bet-skin',
+				  content: $('#bet'),
+				  end: function(){
+					if(dice.bet_value !=0){
+						dice.account = dice.account + dice.bet_value;
+						dice.bet_value = 0;
+					}
+				  }
+				});
+			} else {
+				layer.msg('数据获取失败', {
+					time: 2000,
+					icon: 2
+				});
 			}
-		  }
-		});
+		}, function(e) {
+			layer.msg('数据获取失败', {
+				time: 2000,
+				icon: 2
+			});
+			console.log(e);
+		});	
+
+
+
 	},
 	exit_bet:function(){
 		this.account = this.account + this.bet_value;
@@ -113,11 +133,50 @@ var dice = {
                 });
 			}
 		},
+
+	save_bet:function (number){
+		var index = layer.load();
+		this.ajax.post('/dice/dice-bet', {term:dice.current_term,bet:dice.bet, bet_value:dice.bet_value}, 
+			function(data) {
+				if (data.code == '2018') {
+					$('#show_number').html(dice.bet_name);
+					$('#show_money').html(dice.bet_value);
+					dice.bet_value = 0;
+					layer.closeAll();
+				} else {
+					layer.msg('数据获取失败', {
+						time: 2000,
+						icon: 2
+					});
+				}
+
+			
+			}, function(e) {
+				layer.msg('数据获取失败', {
+					time: 2000,
+					icon: 2
+				});	
+			
+			
+			});
+	},
+	shuoming: function(){
+		layer.open({
+		  type: 1,
+		  title: false,
+		  closeBtn: 0,
+		  area: '420px',
+		  skin: 'layui-layer-nobg', //没有背景色
+		  shadeClose: true,
+		  content: $('#shuoming')
+		});
+	},
 	initPage: function (){
 		if(dice.dicewait == 0){
 			this.ajax.get('/dice/dice-draw', {}, function(data) {
 				if (data.code == '2018') {
-					$('#kjResult').text("第"+data.result.lastTerm+"期:"+data.result.lastResult+"点," + (data.result.lastResult>3?"大":"小"));
+					dice.current_term = data.result.currentTerm;
+					$('#kjResult').text("第"+data.result.lastTerm+"期:"+data.result.lastResult+"点, " + (data.result.lastResult>3?"大":"小") + ", " + (data.result.lastResult%2==0?"双":"单") );
 
 					if(data.result.drawTime == 0){
 						$('#second_show').html("<span id='miao'>抽奖中...</span>");
@@ -128,6 +187,11 @@ var dice = {
 					$("#result_dice").attr("class", 'dice dice_'+data.result.lastResult);
 						
 					dice.dicewait = data.result.drawTime;
+					dice.bet_name = '无';
+					dice.bet_value = 0;
+					dice.bet = 0;
+					$('#show_number').html(dice.bet_name);
+					$('#show_money').html(dice.bet_value);
 					setTimeout(function() {
 						dice.initPage();
 					},
@@ -146,12 +210,12 @@ var dice = {
 				console.log(e);
 			});	
 		}else{
-			this.dicewait--;
-			if(this.dicewait == 0){
+			dice.dicewait--;
+			if(dice.dicewait == 0){
 				$('#second_show').html("<span id='miao'>抽奖中...</span>");
 			}else{
 			
-				$('#miao').html(this.dicewait);
+				$('#miao').html(dice.dicewait);
 			}
 			 
 			setTimeout(function() {
