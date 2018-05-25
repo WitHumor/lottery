90f69af2d9263a7da4a3d2056518cd46faeb845e
zhanzm <zhan.zhiming@successinfo.com.cn>
2018-05-25 20:22:56 +0800

    0525

--- front-end/js/physicalEducation/basketball.js
+++ front-end/js/physicalEducation/basketball.js
@@ -279,6 +279,7 @@ function sureDeal(datastr) {
                 time: 2000
             });
             setTimeout(function() {
+                $('.pourRefreshBtn').click();
                 common.cancelDeal();
             }, 2000);
         } else if (data.code == '1103') {
