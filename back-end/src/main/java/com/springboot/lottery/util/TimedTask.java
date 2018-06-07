6b217018dd628a6b1e25d00e2a77c9f698d55807
zhangzhenghao <10749893@qq.com>
2018-06-07 18:16:24 +0800

    0607返利等...

--- back-end/src/main/java/com/springboot/lottery/util/TimedTask.java
+++ back-end/src/main/java/com/springboot/lottery/util/TimedTask.java
@@ -19,9 +19,12 @@ public class TimedTask {
 	@Autowired
 	private MemberController memberController;
 	
+	/**
+	 * 3秒查找一次赛果
+	 */
 	@Scheduled(fixedDelay = 3000)
 	public void singleNote() {
-		System.out.println(Thread.currentThread().getName() + "定时任务启动");
+		System.out.println(Thread.currentThread().getName() +"足球篮球结算定时任务启动..");
 		boolean member;
 		try {
 			member = memberController.singleNoteAccount();
@@ -32,15 +35,25 @@ public class TimedTask {
 			e.printStackTrace();
 		}
 	}
+	
 	/**
 	 * 每隔一分钟查询超过24小时未结算的结果
 	 */
 	@Scheduled(cron = "0 0/1 * * * ?")
 	public void amidithionOvertime() {
-		System.out.println(Thread.currentThread().getName() + "定时任务启动");
+		System.out.println(Thread.currentThread().getName() + "扫描超过24小时未结算足球篮球定时任务启动..");
 		boolean amidithion = memberController.amidithionOvertime();
 		if(amidithion == false) {
 			System.err.println("无超过24小时未结算数据");
 		}
+	}
+	
+	/**
+	 * 每月一号凌晨1分结算返利金额
+	 */
+	@Scheduled(cron = "0 1 0 1 * ?")
+	public void accountRebate() {
+		System.out.println(Thread.currentThread().getName() + "结算返利金额定时任务启动..");
+		memberController.accountRebate();
 	}
 }
